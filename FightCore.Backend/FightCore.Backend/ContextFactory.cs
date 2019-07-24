a8efa296c575e87d0d23505e8fd7f5cfd56a69e6
Unknown <bartdebever112@gmail.com>
2019-07-24 10:06:46 +0200

    EntityFrameworkCore database set up

---
+++ FightCore.Backend/FightCore.Backend/ContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using FightCore.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace FightCore.Backend
+{
+    /// <inheritdoc />
+    public class ContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        /// <inheritdoc />
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(connectionString, sqlServerOptions => sqlServerOptions.MigrationsAssembly("FightCore.Data"));
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
