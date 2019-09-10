616c0a6e5d8a5ebcb85d2531fa3255acaae03549
Bart de Bever <bartdebever112@gmail.com>
2019-09-10 08:38:04 +0200

    Character controller doc

--- FightCore.Backend/FightCore.Backend/Controllers/CharactersController.cs
+++ FightCore.Backend/FightCore.Backend/Controllers/CharactersController.cs
@@ -8,12 +8,15 @@ using FightCore.Backend.ViewModels.Characters;
 using FightCore.Backend.ViewModels.Errors;
 using FightCore.Models.Characters;
 using FightCore.Services.Games;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace FightCore.Backend.Controllers
 {
+    /// <inheritdoc />
     [Route("[controller]")]
     [ApiController]
     [Produces(HttpContentTypes.APPLICATION_JSON)]
@@ -22,6 +25,7 @@ namespace FightCore.Backend.Controllers
         private readonly ICharacterService _characterService;
         private readonly DbContext _dbContext;
 
+        /// <inheritdoc />
         public CharactersController(
             ICharacterService characterService,
             DbContext dbContext,
@@ -31,15 +35,29 @@ namespace FightCore.Backend.Controllers
             _dbContext = dbContext;
         }
         
+        /// <summary>
+        /// Gets all characters.
+        /// </summary>
+        /// <returns>A list of all characters.</returns>
         [HttpGet]
+        [SwaggerResponse(200, "All characters known to the system.", typeof(List<GetCharacterListViewModel>))]
         public async Task<IActionResult> GetAllCharacters()
         {
             var characters = await _characterService.GetAllWithGamesAsync();
 
-            return MappedOk<List<GetCharacterViewModel>>(characters);
+            return MappedOk<List<GetCharacterListViewModel>>(characters);
         }
 
+        /// <summary>
+        /// Gets a single character based on the provided <paramref name="id"/>.
+        /// </summary>
+        /// <param name="id">
+        /// The id of the <see cref="Character"/> to be searched for.
+        /// </param>
+        /// <returns>The found <see cref="Character"/></returns>
         [HttpGet("{id}")]
+        [SwaggerResponse(200, "The character for the id.", typeof(List<GetCharacterListViewModel>))]
+        [SwaggerResponse(404, "No character found for that id.", typeof(NotFoundErrorViewModel))]
         public async Task<IActionResult> GetCharacter(long id)
         {
             var character = await _characterService.GetWithGameByIdAsync(id);
@@ -52,7 +70,17 @@ namespace FightCore.Backend.Controllers
             return MappedOk<GetCharacterViewModel>(character);
         }
 
+        /// <summary>
+        /// Creates a new character based on the provided body.
+        /// </summary>
+        /// <param name="characterViewModel">
+        /// The character to be created.
+        /// </param>
+        /// <returns>
+        /// Where the character is created.
+        /// </returns>
         [HttpPost]
+        [Authorize]
         public async Task<IActionResult> CreateCharacter(CreateCharacterViewModel characterViewModel)
         {
             var character = Mapper.Map<Character>(characterViewModel);
@@ -63,7 +91,20 @@ namespace FightCore.Backend.Controllers
             return Created("characters", character.Id);
         }
 
+        /// <summary>
+        /// Updates a character based on the provided <paramref name="id"/> and body.
+        /// </summary>
+        /// <param name="id">
+        /// The id of the existing character to override.
+        /// </param>
+        /// <param name="characterViewModel">
+        /// The new values to override it with.
+        /// </param>
+        /// <returns>
+        /// If the action was executed successfully.
+        /// </returns>
         [HttpPut("{id}")]
+        [Authorize]
         public async Task<IActionResult> UpdateCharacter(long id, UpdateCharacterViewModel characterViewModel)
         {
             var trackedCharacter = await _characterService.GetByIdAsync(id);
@@ -71,6 +112,19 @@ namespace FightCore.Backend.Controllers
             if (trackedCharacter == null)
             {
                 return NotFound(NotFoundErrorViewModel.Create(ErrorEntities.CharacterEntity, id));
+            }
+
+            var userId = GetUserIdFromClaims(User);
+
+            if (!userId.HasValue)
+            {
+                return Unauthorized();
+            }
+
+            // If the user is not in the contributors, don't allow him/her to edit the character.
+            if (trackedCharacter.Contributors.All(contributor => contributor.UserId != userId))
+            {
+                return Forbid();
             }
 
             var character = Mapper.Map<Character>(characterViewModel);
