4b8d6355eb7554d1303c2cd627646127fc207519
Bart de Bever <bartdebever112@gmail.com>
2020-04-28 19:59:20 +0200

    Lots of rework and added projects

---
+++ FightCore.Backend/FightCore.Backend/Controllers/MatchupsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FightCore.Backend.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MatchupsController : ControllerBase
+    {
+        [HttpGet("{chracterId}")]
+        public async Task<IActionResult> GetMatchup(long characterId)
+        {
+            return NotFound();
+        }
+    }
+}
